273b9dd160cccb17cfb7ae3b20804751d5ec10a3
MatheusVarella <varellamat@gmail.com>
2021-06-21 21:37:50 -0300

    install expo landing

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { useFonts } from "@expo-google-fonts/inter";
+import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import { Rajdhani_500Medium, Rajdhani_700Bold} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
 
 import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
-  useFonts({
+  const [fontsLoaded] = useFonts({ 
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
     Rajdhani_700Bold
   });
+
+  if(!fontsLoaded){ //Verifica se as fontes foram carregadas utilizando uma biblioteca do expo app loading
+    return <AppLoading />
+  }
 
   return(
     <SignIn />
