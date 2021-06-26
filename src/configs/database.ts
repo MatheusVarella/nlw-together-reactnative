b6eb300ac05e30ef18cfd551f9eceeb3593e6048
MatheusVarella <varellamat@gmail.com>
2021-06-26 16:48:51 -0300

    SAving authentication data

---
+++ src/configs/database.ts
@@ -0,0 +1,11 @@
+const DATABASE_NAME = "@gameplay";
+
+//@app:colletion nome para padronizar a coleção 
+
+const COLLECTION_USER = `${DATABASE_NAME}:user`;
+const COLLECTION_APPOINTMENT = `${DATABASE_NAME}:appointment`;
+
+export {
+    COLLECTION_USER,
+    COLLECTION_APPOINTMENT
+}
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -4,10 +4,11 @@ import React,
   useContext,
   useState,
   ReactNode,
+  useEffect
 } from "react";
 
 import * as AuthSession from "expo-auth-session";
-
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Biblioteca para salvar os dados do usuário
 
 const { REDIRECT_URI } = process.env;
 const { SCOPE } = process.env;
@@ -16,6 +17,7 @@ const { CLIENTE_ID } = process.env;
 const { CDN_IMAGE } = process.env; 
 
 import { api } from "../services/api";
+import { COLLECTION_USER } from "../configs/database";
 
 interface User {
   id: string;
@@ -66,11 +68,16 @@ function AuthProvider({ children }:  AuthProviderProps) {
         const firstName = userInfo.data.username.split(' ')[0]; // Pega o primeiro nome
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
         
-        setUser({
+        // salvando os dados do usuário no dispositivo
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        }); 
+        }
+
+        await AsyncStorage.setItem(COLLECTION_USER, JSON.stringify(userData));
+
+        setUser(userData); 
       } 
     } catch {
       throw new Error("Não foi possível autenticar");
@@ -78,6 +85,22 @@ function AuthProvider({ children }:  AuthProviderProps) {
       setLoading(false);
     }
   }
+
+  async function  loadUserStorageData() {
+    const storage = await AsyncStorage.getItem(COLLECTION_USER);
+
+    if(storage) {
+      const userLogged = JSON.parse(storage) as User;
+
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ 
